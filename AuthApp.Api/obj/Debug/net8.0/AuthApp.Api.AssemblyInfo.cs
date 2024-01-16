6d0793a2a1966fe75fea2698be8a7abdbb12d979
alireza jabari <alirezajabari.ir@gmail.com>
2024-01-16 02:26:25 -0800

    remove extra files

--- AuthApp.Api/obj/Debug/net8.0/AuthApp.Api.AssemblyInfo.cs
+++ AuthApp.Api/obj/Debug/net8.0/AuthApp.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed761a5c46b17e91abc47e3d528f6c751626d6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3075c88cbc347262ee41cf5056c8f1f371c5bc16")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthApp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthApp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
